0c9ecbca31142bc40117813324f468fc66bed261
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-29 16:12:54 +0200

    feat: generate 10 random emails

--- js/script.js
+++ js/script.js
@@ -8,19 +8,18 @@ createApp({
         };
     },
     created() {
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((email) => {
-            console.log(email);
-            this.emails = email.data.response
-        });
+        this.generateEmail()
     },
     methods: {
-        generateEmail: function () {                
+        generateEmail: function () {
+            for (i = 0; i < 10; i++) {
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((email) => {
                     // console.log("random email", email.data.response);
                     this.emails = email.data.response;
                     this.emailsArray.push(this.emails);
                 })
             }
+        }
 
-        },
+    },
 }).mount('#app')
